27652f87df365d4310261308c971edff16f07e19
Anderis <88600844+Anderis@users.noreply.github.com>
2023-06-05 20:54:18 -0500

    Push Changes
    Adding Entry Points

--- WebApp/timer.js
+++ WebApp/timer.js
@@ -1,1 +1,24 @@
-
+function addTask() {
+    var taskContainer = document.getElementById("taskContainer");
+    var taskCount = taskContainer.children.length / 3 + 1;
+    var taskLabel = document.createElement("label");
+    taskLabel.for = "task" + taskCount;
+    taskLabel.innerHTML = taskCount + ".";
+    var taskInput = document.createElement("input");
+    taskInput.type = "text";
+    taskInput.id = "task" + taskCount;
+    taskInput.className = "taskInput";
+    taskContainer.appendChild(taskLabel);
+    taskContainer.appendChild(taskInput);
+    taskContainer.appendChild(document.createElement("br"));
+    taskInput.focus();
+    taskInput.onkeyup = function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            var task = document.getElementById("task" + taskCount);
+            task.innerHTML = taskInput.value;
+            taskInput.value = "";
+            taskInput.focus();
+        }
+    };
+}
